94eecc33eb07a3479ec6505e331713eec5a6fc35
Sunny Pacheco <pacheco.sunny@gmail.com>
2021-02-11 15:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function theBeatlesPlay (musicians, instruments) {
 
 function johnLennonFacts (facts) {
   let newArray = []
+  let i=0
   while (i < facts.length) {
     newArray.push(`${facts[i]}!!!`);
     i++
